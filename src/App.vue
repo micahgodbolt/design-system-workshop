369683887e1242947fce55983cb1a023d7afdbbf
Micah Godbolt <micahgodbolt@gmail.com>
2017-08-23 08:48:19 -0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/inputs/Button/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="ds-button" @click="onClick"> 
+        <slot></slot> 
+    </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    methods: {
+      onClick () {
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+$color: red;
+.ds-button {
+    background: $color;
+}
+</style>
