7eda1ebe55db495583f7c7c9f9aaf48d1335200e
Micah Godbolt <micahgodbolt@gmail.com>
2017-08-30 23:50:36 -0700

    Textfield

---
+++ src/components/inputs/Textfield/TextField.vue
@@ -0,0 +1,29 @@
+<template>
+   <input class="new-todo"
+    autofocus 
+    autocomplete="off"
+    placeholder="What needs to be done?"
+    v-model="newTodo"
+    @keyup.enter="keyupEnter">
+</template>
+
+<script>
+export default {
+  name: 'TextField',
+  props: ['model'],
+  data: function () {
+    return {
+      newTodo: ''
+    }
+  },
+  methods: {
+    keyupEnter: function (newTodo) {
+      this.$emit('keyupEnter', newTodo)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
